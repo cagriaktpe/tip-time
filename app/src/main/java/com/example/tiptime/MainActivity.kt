1724216b45dd1d3fb675a3e04c3455f9ae1e39e9
Samet Çağrı AKTEPE <blackorsaktepe@gmail.com>
2022-03-19 19:36:55 +0300

    edited styles and themes

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
